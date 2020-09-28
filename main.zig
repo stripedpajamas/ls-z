66aa39bffcbaf428a102e3a3c603e05c6606c72e
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-09-28 09:54:29 -0700

    Small update

--- main.zig
+++ main.zig
@@ -1,20 +1,34 @@
 const std = @import("std");
 const fs = std.fs;
+const heap = std.heap;
+const process = std.process;
+
+const open_opts = fs.Dir.OpenDirOptions{ .iterate = true };
 
 pub fn main() !void {
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    const args = try process.argsAlloc(allocator);
+    defer process.argsFree(allocator, args);
     const cwd = fs.cwd();
-    const cwd_dir = try cwd.openDir(".", .{});
-    var it = cwd_dir.iterate();
+    var target_dir = if (args.len < 2) cwd.openDir(".", open_opts) else cwd.openDir(args[1], open_opts);
 
-    while (try it.next()) |entry| {
-        const name = entry.name;
-        const kind = entry.kind;
+    if (target_dir) |*dir| {
+        defer dir.close();
+        var it = dir.iterate();
+        while (try it.next()) |entry| {
+            const name = entry.name;
+            const kind = entry.kind;
 
-        if (kind == fs.Dir.Entry.Kind.Directory) {
-            std.debug.warn("dir : {}\n", .{name});
-        } else if (kind == fs.Dir.Entry.Kind.File) {
-            std.debug.warn("file: {}\n", .{name});
+            if (kind == fs.Dir.Entry.Kind.Directory) {
+                std.debug.warn("dir : {}\n", .{name});
+            } else if (kind == fs.Dir.Entry.Kind.File) {
+                std.debug.warn("file: {}\n", .{name});
+            }
         }
+    } else |err| {
+        std.debug.warn("err! {}\n", .{err});
     }
 }
-
